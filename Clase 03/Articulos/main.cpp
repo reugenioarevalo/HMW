92b674a61362e366753cc06ac78dc9e261915940
walterstamm <walterstamm92@gmail.com>
2020-09-18 19:19:26 -0300

    prueba de borrar codigo

--- Clase 03/Articulos/main.cpp
+++ Clase 03/Articulos/main.cpp
@@ -1,11 +1,4 @@
 #include <iostream>
 using namespace std;
 #include <cstdlib>
-#include "articulo.h"
-int main(){
-  Articulo v[3];
-  cargar_articulos(v, 3);
-  system("cls");
-  mostrar_articulos(v, 3);
-  return 0;
-}
+#include "articulo.
